0f72ef74720e343501f76cf3ae5a62a22f4b1bd2
CyberNstt <122619691+CyberNstt@users.noreply.github.com>
2023-03-07 12:09:12 +0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,89 @@
+// Function for first question
+function openFunctionFirst() {
+	document.getElementById('QuestLimiterAnswer1').style.display = 'flex';
+	document.getElementById('QuestLimiterAnswer1').style.opacity = '1';
+	document.getElementById('QuestBtnOpen1').style.display = 'none';
+	document.getElementById('QuestBtnClose1').style.display = 'block';
+	document.getElementById('QuestBtnClose1').style.opacity = '1';
+	document.getElementById('QuestLimiterMatter1').style.fontWeight = '700';
+}// Function for first question
+function closeFunctionFirst() {
+	document.getElementById('QuestLimiterAnswer1').style.display = 'none';
+	document.getElementById('QuestLimiterAnswer1').style.opacity = '0';
+	document.getElementById('QuestBtnOpen1').style.display = 'block';
+	document.getElementById('QuestBtnClose1').style.display = 'none';
+	document.getElementById('QuestBtnClose1').style.opacity = '0';
+	document.getElementById('QuestLimiterMatter1').style.fontWeight = '500';
+}
+
+// Function for second question
+function openFunctionSecond() {
+	document.getElementById('QuestLimiterAnswer2').style.display = 'flex';
+	document.getElementById('QuestLimiterAnswer2').style.opacity = '1';
+	document.getElementById('QuestBtnOpen2').style.display = 'none';
+	document.getElementById('QuestBtnClose2').style.display = 'block';
+	document.getElementById('QuestBtnClose2').style.opacity = '1';
+	document.getElementById('QuestLimiterMatter2').style.fontWeight = '700';
+}// Function for second question
+function closeFunctionSecond() {
+	document.getElementById('QuestLimiterAnswer2').style.display = 'none';
+	document.getElementById('QuestLimiterAnswer2').style.opacity = '0';
+	document.getElementById('QuestBtnOpen2').style.display = 'block';
+	document.getElementById('QuestBtnClose2').style.display = 'none';
+	document.getElementById('QuestBtnClose2').style.opacity = '0';
+	document.getElementById('QuestLimiterMatter2').style.fontWeight = '500';
+}
+
+// Function for third question
+function openFunctionThird() {
+	document.getElementById('QuestLimiterAnswer3').style.display = 'flex';
+	document.getElementById('QuestLimiterAnswer3').style.opacity = '1';
+	document.getElementById('QuestBtnOpen3').style.display = 'none';
+	document.getElementById('QuestBtnClose3').style.display = 'block';
+	document.getElementById('QuestBtnClose3').style.opacity = '1';
+	document.getElementById('QuestLimiterMatter3').style.fontWeight = '700';
+}// Function for third question
+function closeFunctionThird() {
+	document.getElementById('QuestLimiterAnswer3').style.display = 'none';
+	document.getElementById('QuestLimiterAnswer3').style.opacity = '0';
+	document.getElementById('QuestBtnOpen3').style.display = 'block';
+	document.getElementById('QuestBtnClose3').style.display = 'none';
+	document.getElementById('QuestBtnClose3').style.opacity = '0';
+	document.getElementById('QuestLimiterMatter3').style.fontWeight = '500';
+}
+
+// Function for fourth question
+function openFunctionFourth() {
+	document.getElementById('QuestLimiterAnswer4').style.display = 'flex';
+	document.getElementById('QuestLimiterAnswer4').style.opacity = '1';
+	document.getElementById('QuestBtnOpen4').style.display = 'none';
+	document.getElementById('QuestBtnClose4').style.display = 'block';
+	document.getElementById('QuestBtnClose4').style.opacity = '1';
+	document.getElementById('QuestLimiterMatter4').style.fontWeight = '700';
+}// Function for fourth question
+function closeFunctionFourth() {
+	document.getElementById('QuestLimiterAnswer4').style.display = 'none';
+	document.getElementById('QuestLimiterAnswer4').style.opacity = '0';
+	document.getElementById('QuestBtnOpen4').style.display = 'block';
+	document.getElementById('QuestBtnClose4').style.display = 'none';
+	document.getElementById('QuestBtnClose4').style.opacity = '0';
+	document.getElementById('QuestLimiterMatter4').style.fontWeight = '500';
+}
+
+// Function for fifth question
+function openFunctionFifth() {
+	document.getElementById('QuestLimiterAnswer5').style.display = 'flex';
+	document.getElementById('QuestLimiterAnswer5').style.opacity = '1';
+	document.getElementById('QuestBtnOpen5').style.display = 'none';
+	document.getElementById('QuestBtnClose5').style.display = 'block';
+	document.getElementById('QuestBtnClose5').style.opacity = '1';
+	document.getElementById('QuestLimiterMatter5').style.fontWeight = '700';
+}// Function for fifth question
+function closeFunctionFifth() {
+	document.getElementById('QuestLimiterAnswer5').style.display = 'none';
+	document.getElementById('QuestLimiterAnswer5').style.opacity = '0';
+	document.getElementById('QuestBtnOpen5').style.display = 'block';
+	document.getElementById('QuestBtnClose5').style.display = 'none';
+	document.getElementById('QuestBtnClose5').style.opacity = '0';
+	document.getElementById('QuestLimiterMatter5').style.fontWeight = '500';
+}
